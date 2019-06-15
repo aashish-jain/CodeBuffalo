038a82a81101cdba7d6b00b83006e8262a3a140f
Dhayanidhi <dnidhi2710@gmail.com>
2019-06-15 14:56:06 -0400

    front _end template import

--- front_end/App.tsx
+++ front_end/App.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { View, StyleSheet } from 'react-native';
+import MainNavigator from './src/navigators';
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+  return <MainNavigator />;
 }
 
 const styles = StyleSheet.create({
